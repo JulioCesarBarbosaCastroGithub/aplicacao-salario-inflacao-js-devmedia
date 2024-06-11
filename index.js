83b910e4fa84f3d230ca5a9693d3b807a9941f26
JulioCesarBarbosaCastroGithub <juliocesarbarbosacastro@outlook.com>
2024-06-11 18:10:28 -0300

    second commit

--- index.js
+++ index.js
@@ -8,47 +8,109 @@ console.log("3 - Comparação entre o percentual de aumento salarial e o IPCA");
 
 let opcao = entradaDados.question("\nDigite o numero da sua escolha: ");
 
-let salarioMinimo = {
-    ano2010: 510.00,
-    ano2011: 545.00,
-    ano2012: 622.00,
-    ano2013: 678.00,
-    ano2014: 724.00,
-    ano2015: 788.00,
-    ano2016: 880.00,
-    ano2017: 937.00,
-    ano2018: 954.00,
-    ano2019: 998.00,
-    ano2020: 1045.00
-};
+opcao = Number(opcao);
 
-let inflacaoIpca = {
-    ano2010: 5.91,
-    ano2011: 6.50,
-    ano2012: 5.84,
-    ano2013: 5.91,
-    ano2014: 6.41,
-    ano2015: 10.67,
-    ano2016: 6.29,
-    ano2017: 2.95,
-    ano2018: 3.75,
-    ano2019: 4.31,
-    ano2020: 4.52
-};
+let salarioMinimo = [
+    {ano: 2010, salario: 510.00,},
+    {ano: 2011, salario: 545.00},
+    {ano: 2012, salario: 622.00},
+    {ano: 2013, salario: 678.00},
+    {ano: 2014, salario: 724.00},
+    {ano: 2015, salario: 788.00},
+    {ano: 2016, salario: 880.00},
+    {ano: 2017, salario: 937.00},
+    {ano: 2018, salario: 954.00},
+    {ano: 2019, salario: 998.00},
+    {ano: 2020, salario: 1045.00}
+];
 
-let diferenca = salarioMinimo.ano2011 - salarioMinimo.ano2010;
+let inflacao = [
+    {ano: 2010, ipca: 5.91},
+    {ano: 2011, ipca: 6.50},
+    {ano: 2012, ipca: 5.84},
+    {ano: 2013, ipca: 5.91},
+    {ano: 2014, ipca: 6.41},
+    {ano: 2015, ipca: 10.67},
+    {ano: 2016, ipca: 6.29},
+    {ano: 2017, ipca: 2.95},
+    {ano: 2018, ipca: 3.75},
+    {ano: 2019, ipca: 4.31},
+    {ano: 2020, ipca: 4.52}
+];
 
-let percentualCrescimento = (diferenca / salarioMinimo[2010]) * 100;
+let anoLabel = "Ano: ";
+let salarioLabel = "Salário mínimo: ";
+let crescimentoLabel = "Crescimento Salarial: ";
+let ipcaLabel = "Inflação IPCA: ";
 
-if(opcao == 1){
+anoLabel = anoLabel.padEnd(30,'.');
+salarioLabel = salarioLabel.padEnd(30,'.');
+crescimentoLabel = crescimentoLabel.padEnd(30,'.');
+ipcaLabel = ipcaLabel.padEnd(30,'.');
 
+switch(opcao)
+{
+    case 1:
+        for(let i = 0; i <= salarioMinimo.length-1; i++)
+        {
+            let ano = salarioMinimo[i].ano;
+            let salario = salarioMinimo[i].salario;
+
+            let salarioFormatado = salario.toFixed(2).replace(".",",");
+
+            console.log("\n");
+            console.log(anoLabel+ano);
+            console.log(salarioLabel+"R$ "+salarioFormatado);
+        }
+        break;
+    case 2:
+        for(let i = 0; i <= inflacao.length-1; i++)
+        {
+            let ano = inflacao[i].ano;
+            let ipca = inflacao[i].ipca;
+
+            let ipcaFormatado = ipca.toFixed(2).replace(".",",");
+
+            console.log("\n");
+            console.log(anoLabel+ano);
+            console.log(ipcaLabel+ipcaFormatado+"%");
+        }
+        break;
+    case 3:
+        for(let i = 0; i <= salarioMinimo.length-1; i++)
+        {
+            let ano = salarioMinimo[i].ano;
+            let salario = salarioMinimo[i].salario;
+            let percentualCrescimento;
+            let crescimentoFormatado;
+
+            if(i > 0)
+            {
+                let salarioAnterior = salarioMinimo[i-1].salario;
+                let diferenca = salario - salarioAnterior;
+
+                percentualCrescimento = (diferenca / salarioAnterior) * 100;
+
+                crescimentoFormatado = percentualCrescimento.toFixed(2).replace(".",",")+"%";
+            }
+            else
+            {
+                crescimentoFormatado = "-";
+            }
+
+            let ipca = inflacao[i].ipca;
+
+            let salarioFormatado = salario.toFixed(2).replace(".",",");
+            let ipcaFormatado = ipca.toFixed(2).replace(".",",");
+
+            console.log("\n");
+            console.log(anoLabel+ano);
+            console.log(salarioLabel+"R$ "+salarioFormatado);
+            console.log(crescimentoLabel+crescimentoFormatado);
+            console.log(ipcaLabel+ipcaFormatado+"%");
+        }
+        break;
+    default:
+        console.log("Opção Inválida!");
+        break;
 }
-else if(opcao == 2){
-
-}
-else if(opcao == 3){
-
-}
-else{
-    console.log("Opção inválida, tente novamente!");
-};
